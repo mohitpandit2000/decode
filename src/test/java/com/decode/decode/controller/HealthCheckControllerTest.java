97f0fa54e11fdc312a1504afd1a3e69fc1803175
mohitpandit2000 <mohit1pandey15@gmail.com>
2025-04-09 10:25:13 +0530

    add in test

---
+++ src/test/java/com/decode/decode/controller/HealthCheckControllerTest.java
@@ -0,0 +1,11 @@
+package com.decode.decode.controller;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class HealthCheckControllerTest {
+    @BeforeAll
+    public static void init(){
+        System.out.println("hello");
+    }
+
+}
