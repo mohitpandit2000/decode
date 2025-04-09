578aa616a65bd615207674a5f24c0d3d9d8896b6
mohitpandit2000 <mohit1pandey15@gmail.com>
2025-04-09 10:30:13 +0530

    added before each

--- src/test/java/com/decode/decode/controller/HealthCheckControllerTest.java
+++ src/test/java/com/decode/decode/controller/HealthCheckControllerTest.java
@@ -1,11 +1,16 @@
 package com.decode.decode.controller;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class HealthCheckControllerTest {
     @BeforeAll
     public static void init(){
         System.out.println("hello");
+    }
+    @BeforeEach
+    public void initialize(){
+        System.out.println("initialization");
     }
 
 }
