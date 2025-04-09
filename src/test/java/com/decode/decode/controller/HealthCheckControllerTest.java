200f874ebfa900f51b93158f5af2c59a92e450fc
mohitpandit2000 <mohit1pandey15@gmail.com>
2025-04-09 12:44:20 +0530

    Revert "second commit of sqash"
    This reverts commit 48ed4a3355afda4d8949b54271215bf3f2283af4.

--- src/test/java/com/decode/decode/controller/HealthCheckControllerTest.java
+++ src/test/java/com/decode/decode/controller/HealthCheckControllerTest.java
@@ -12,9 +12,6 @@ public class HealthCheckControllerTest {
     public void initialize(){
         System.out.println("initialization");
         System.out.println("first commit");
-        System.out.println("second commit");
-        System.out.println("third commit");
-        System.out.println("fourth commit");
     }
 
 }
